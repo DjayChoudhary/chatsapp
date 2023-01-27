147ecb38f57afaf5f44416828415a98f7f8f7450
Dhananjay Choudhary <40604207+DjayChoudhary@users.noreply.github.com>
2023-01-27 23:56:53 +0530

    created POJO class for message

---
+++ src/main/java/io/djay/chatsapp/Message.java
@@ -0,0 +1,25 @@
+package io.djay.chatsapp;
+
+public class Message {
+    private String name;
+
+    public Message() {
+    }
+
+    public Message(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Message [name=" + name + "]";
+    }
+}
