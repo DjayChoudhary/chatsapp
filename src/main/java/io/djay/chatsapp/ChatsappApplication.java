dca5629d711c0d19bfe1344f600ded0cf2cf052c
Dhananjay Choudhary <40604207+DjayChoudhary@users.noreply.github.com>
2023-01-27 23:56:12 +0530

    initial commit

---
+++ src/main/java/io/djay/chatsapp/ChatsappApplication.java
@@ -0,0 +1,13 @@
+package io.djay.chatsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatsappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/djay/chatsapp/ChatsappApplicationTests.java
@@ -0,0 +1,13 @@
+package io.djay.chatsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
