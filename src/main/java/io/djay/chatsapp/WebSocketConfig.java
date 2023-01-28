db4511191817a7f4b2e3f0742f1a52a7b0053cec
Dhananjay Choudhary <40604207+DjayChoudhary@users.noreply.github.com>
2023-01-28 23:33:29 +0530

    update WebSocketConfig

--- src/main/java/io/djay/chatsapp/WebSocketConfig.java
+++ src/main/java/io/djay/chatsapp/WebSocketConfig.java
@@ -1,5 +1,7 @@
 package io.djay.chatsapp;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -13,6 +15,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
+
         registry.setApplicationDestinationPrefixes("/app");
     }
 
